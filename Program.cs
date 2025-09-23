0f2a5d51b1aa135eaa0d8c65105953757347e21a
DiegoPmz <diego.pmz08@gmail.com>
2025-09-23 02:19:35 -0600

    Agregar autenticación JWT y soporte CORS -Se actualizó KanbanAppApi.csproj para incluir UserSecretsId y se agregó el paquete de autenticación JWT. -Se mejoró Program.cs con nuevas directivas using, se ampliaron los registros de servicios para repositorios y servicios de gestión de usuarios, y se configuró CORS para desarrollo local. -Se implementó la autenticación JWT con validación de tokens y middleware para el manejo seguro de solicitudes.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using KanbanAppApi.Data;
+using KanbanAppApi.Repositories;
+using KanbanAppApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +18,59 @@ builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationContextDB>(options => options.UseSqlServer(connectionString));
 
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IUserRepository, UserRespository>();
+builder.Services.AddScoped<IBoardRepository, BoardRepository>();
+builder.Services.AddScoped<IColumnRepository, ColumnRepository>();
+builder.Services.AddScoped<IBoardTaskRepository, BoardTaskRepository>();
+builder.Services.AddScoped<ISubTaskRepository, SubTaskRepository>();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddCors(options =>
+{
+   options.AddDefaultPolicy(policy =>
+   {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+var secretKey = builder.Configuration["JwtSecretKey"]!;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+   .AddJwtBearer(options =>
+   {
+       options.TokenValidationParameters = new TokenValidationParameters
+       { 
+           ValidateIssuer = true,
+           ValidIssuer = "KanbanAppApi",
+           ValidateAudience = true,
+           ValidAudience = "KanbanApp",
+           ValidateLifetime = true,
+           ClockSkew = TimeSpan.Zero,
+           ValidateIssuerSigningKey = true,
+           IssuerSigningKey = new SymmetricSecurityKey(
+               Encoding.UTF8.GetBytes(secretKey)
+           )
+       };
+
+       options.Events = new JwtBearerEvents
+       {
+           OnMessageReceived = context =>
+           {
+               if (context.Request.Cookies.TryGetValue("auth_token", out var token))
+               {
+                   context.Token = token;
+               }
+               return Task.CompletedTask;
+           }
+       };
+   });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +80,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
